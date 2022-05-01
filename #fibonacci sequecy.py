6ace507f60e7a82df30a4c197e2aa9ca5cae7163
Sarleymwaka <97435779+Sarleymwaka@users.noreply.github.com>
2022-05-01 22:14:14 +0300

    Add files via upload

---
+++ #fibonacci sequecy.py
@@ -0,0 +1,14 @@
+#fibonacci sequency
+print("welcome")
+number1=input("enter number 1: ")
+print (number1)
+number2=input("enter number 2: ")
+print (number2)
+number3=input("enter number 3: ")
+print (number3)
+if (int(number3)==(int(number1)+int(number2))):
+ print("sequence is fibonacci")
+else:
+  (int(number3)!=(int(number1)+int(number2)))
+  print("sequence is not fibonacci")
+
---
+++ #prime numbers.py
@@ -0,0 +1,8 @@
+#prime numbers
+print("welcome")
+number=int(input("enter number: "))
+if (number%number)==1:
+  print("the number is prime")
+elif(number%2)==0:
+  print("the number is not prime")
+  
